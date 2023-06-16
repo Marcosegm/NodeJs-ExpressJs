218a9abf37fe0b61d3e11e30d7e193fe0ec2f7be
Marcosegm <marcos-gm14@hotmail.com>
2023-06-16 11:10:28 -0500

    Subiendo proyecto

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cors = require("cors")
+const routerApi = require("./routes")
+const {logErrors, errorHandler, boomErrorHandler} = require("./middlewares/error.handler")
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whiteList = ["http://localhost:3000/"]
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error("No permitido"))
+    }
+  }
+}
+app.use(cors(options))
+
+app.get('/',(req, res) => {
+  res.send("Hola mi server en express")
+})
+
+app.get('/nueva-ruta',(req, res) => {
+  res.send("Hola soy un endPoint")
+})
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
